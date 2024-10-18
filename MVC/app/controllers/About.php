0fda9600fd9d719088fab87661e0de0084db3b98
MelindaSulaeman <melindasulaeman08@gmail.com>
2024-10-19 01:18:27 +0700

    Video 5 - View

--- MVC/app/controllers/About.php
+++ MVC/app/controllers/About.php
@@ -1,10 +1,20 @@
 <?php
 
-class About {
-    public function index ($nama, $pekerjaan) {
-        echo "Hallo, nama saya $nama saya adalah seorang $pekerjaan";
+class About extends Controller {
+    public function index ($nama = 'Melinda', $Pekerjaan = 'Mahasiswa', $umur = 20) 
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $Pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page() {
-        echo'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
