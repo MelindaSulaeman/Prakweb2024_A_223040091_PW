9e1f8d0e76ffdedb74448540fe9a01c0bc70615e
MelindaSulaeman <melindasulaeman08@gmail.com>
2024-10-19 23:43:59 +0700

    Video 8 - Database Wrapper

---
+++ MVC/app/config/config.php
@@ -0,0 +1,9 @@
+<?php  
+
+define('BASEURL', 'http://localhost/Praktikum%20Web/Prakweb2024_A_223040091_PW/MVC/public/');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
--- MVC/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php  
-
-define('BASEURL', 'http://localhost/Praktikum%20Web/Prakweb2024_A_223040091_PW/MVC/public/');
